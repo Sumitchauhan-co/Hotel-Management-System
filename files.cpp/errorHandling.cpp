ca3d50e8d3fefba73929f4cd15c349ddba807313
Sumitchauhan-co <chauhan.sumit3012@gmail.com>
2025-08-11 19:32:45 +0530

    update 3.64

--- files.cpp/errorHandling.cpp
+++ files.cpp/errorHandling.cpp
@@ -7,6 +7,18 @@ void errorHandling::input(string& val)
     getline(cin>>ws,val);
 }
 
+void errorHandling::input(size_t& val)
+{
+    cin >> val;
+    while(cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout<<"\nInvalid input, try again!\n";
+        cin>>val;
+    }
+}
+
 void errorHandling::input(int& val)
 {
     cin >> val;
--- files.h/errorHandling.h
+++ files.h/errorHandling.h
@@ -8,6 +8,7 @@ class errorHandling
 public:
     static void input(string& val);
     static void input(int& val);
+    static void input(size_t& val);
     static void input(char& val);
     static void input(double& val);
     static void input(bool& val);
